123d5663939998dfdce3c1b3a7bbf74654a98ec8
Muhammad Hamza Samad <160007582+muhammadhamza718@users.noreply.github.com>
2026-01-31 17:08:48 +0500

    Uncomment day selection logic in f-string.py

--- Ecic-Class-Work/01_day/f-string.py
+++ Ecic-Class-Work/01_day/f-string.py
@@ -19,23 +19,23 @@ else:
     print("The number is Odd")
 
 
-# day = int(input("Enter the number: "))
-# if day == 1:
-#     print("Monday")
-# elif day == 2:
-#     print("Tuesday")
-# elif day == 3:
-#     print("Wednesday")
-# elif day == 4:
-#     print("Thursday")
-# elif day == 5:
-#     print("Friday")
-# elif day == 6:
-#     print("Saturday")
-# elif day == 7:
-#     print("Sunday")
-# else:
-#     print("unknown day")
+day = int(input("Enter the number: "))
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
+else:
+    print("unknown day")
 
 
 Marks=int(input("Enter your numbers= "))
