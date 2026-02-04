9beeb33f189d13eac1ae680effc1faa6a33f3673
muhammadhamza718 <mhamza77188@gmail.com>
2026-02-04 11:15:16 +0500

    17_day work

--- Ecic-Class-Work/01_day/f-string.py
+++ Ecic-Class-Work/01_day/f-string.py
@@ -1,41 +1,41 @@
-# price = 250 
-# print(f"The total price is Rs.{price}")
+price = 250 
+print(f"The total price is Rs.{price}")
 
 city = "karachi"
 print(f"Welcome to {city}")
 
-# x = 4
-# y = 6
-# print(f"Product of {x + y}")
+x = 4
+y = 6
+print(f"Product of {x + y}")
 
-# age = 18
-# if age >= 18:
-#     print("Eligible to vote")
+age = 18
+if age >= 18:
+    print("Eligible to vote")
 
-# a = int(input("Enter the value= "))
-# if a%2 <= 0:
-#     print("The number is Even")
-# else:
-#     print("The number is Odd")
+a = int(input("Enter the value= "))
+if a%2 <= 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
 
 
-# day = int(input("Enter the number: "))
-# if day == 1:
-#     print("Monday")
-# elif day == 2:
-#     print("Tuesday")
-# elif day == 3:
-#     print("Wednesday")
-# elif day == 4:
-#     print("Thursday")
-# elif day == 5:
-#     print("Friday")
-# elif day == 6:
-#     print("Saturday")
-# elif day == 7:
-#     print("Sunday")
-# else:
-#     print("unknown day")
+day = int(input("Enter the number: "))
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("Tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("Friday")
+elif day == 6:
+    print("Saturday")
+elif day == 7:
+    print("Sunday")
+else:
+    print("unknown day")
 
 
 Marks=int(input("Enter your numbers= "))
@@ -55,11 +55,11 @@ elif Marks <= 100:
 else:
     print("unknown number")
 
-# number=int(input("Enter your number: "))
-# if number%2:
-#     print("the number is Even")
-# else:
-#     print("the number is odd")
+number=int(input("Enter your number: "))
+if number%2:
+    print("the number is Even")
+else:
+    print("the number is odd")
 
 units=int(input("Enter your unit= "))
 if units <= 100:
